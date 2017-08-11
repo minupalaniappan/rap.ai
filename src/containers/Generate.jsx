16b90f6d663b85597e4ecb2dea6dc78e1588bc82
Minu Palaniappan <mspal@ucdavis.edu>
2017-08-11 19:28:56 -0400

    adding generate

--- src/containers/Generate.jsx
+++ src/containers/Generate.jsx
@@ -4,14 +4,23 @@ import $ from 'jquery'
 
 var RAPGENIUS_ACCESS_TOKEN = "WJCDe9kbzfhPQnwzBXQcMw8JExlRMefEWN19V0elsOMNhriiJodaO9Ld6hQ2TZn9";
 var ARTIST_SEARCH_ENDPOINT = decodeURIComponent('https%3A%2F%2Fapi.genius.com%2Fartists');
-
+var DATA;
 
 export default class Generate extends Component {
 	constructor (props) {
 		super(props);
+		this.state = {
+			data: ""
+		}
 	}
 
 	componentDidMount() {
+		var that = this;
+		getData(this.props.params.artist, (data) => {
+			that.setState({
+				data: data.response.artist
+			});
+		});
 	}
 
 	componentWillMount() {
@@ -24,23 +33,21 @@ export default class Generate extends Component {
 	}
 
 	render() {
-		var artist = getData(this.props.params.artist);
-		return (<div>{this.props.params.artist}</div>)
+		return (<div></div>);
 	}
 }
 
-var getData = (id) => {
+var getData = (id, callback) => {
 	var that = this;
 	$.ajax({
-		url: ARTIST_SEARCH_ENDPOINT,
+		url: ARTIST_SEARCH_ENDPOINT+"/"+id,
 		type: "GET", 
 		dataType: 'json',
 		data: {
 			"access_token": RAPGENIUS_ACCESS_TOKEN,
-			"id": id
 		},
 		success: (data) => {
-			console.log(data);
+			callback(data);
 		}
 	});
 }
